11f9a088253eeea6fba9823faf6473311aaa60bc
Edward Ly <esly14@earlham.edu>
2016-12-07 21:53:33 -0500

    colorful fireworks with proper trails

--- demo.cpp
+++ demo.cpp
@@ -16,7 +16,7 @@ struct GLintPoint {
 };
 
 // global variables
-const int WINDOW_WIDTH = 640, WINDOW_HEIGHT = 480, MAX_FIREWORKS = 15;
+const int WINDOW_WIDTH = 640, WINDOW_HEIGHT = 480, MAX_FIREWORKS = 10, MAX_FRAMES = 5;
 GLdouble windowLeft = 0.0, windowRight = 640.0, windowBottom = 0.0, windowTop = 480.0;
 
 Firework fireworks[MAX_FIREWORKS];
@@ -39,29 +39,9 @@ void myInit() {
 	gluOrtho2D(windowLeft, windowRight, windowBottom, windowTop);
 }
 
-void timer(int arg) {
-	// update firework state
-    for (int i = 0; i < MAX_FIREWORKS; i++) {
-        fireworks[i].explode();
-    }
-
-	glutPostRedisplay(); // redraw display
-}
-
-void myDisplay() {
-    glAccum(GL_RETURN, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT); // clear the screen
-
-    glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-	// Displacement trick for exact pixelisation
-	glTranslatef(0.375, 0.375, 0);
-
-	// draw the fireworks
+void draw_fireworks() {
     for (int loop = 0; loop < MAX_FIREWORKS; loop++)
 	{
-        // Set the point size of the firework particles (this needs to be called BEFORE opening the glBegin(GL_POINTS) section!)
         glPointSize(fireworks[loop].particleSize);
         glColor4f(fireworks[loop].red, fireworks[loop].green, fireworks[loop].blue, fireworks[loop].alpha);
 
@@ -72,8 +52,34 @@ void myDisplay() {
 		}
         glEnd();
 	}
+}
+
+void timer(int arg) {
+	// update firework state
+    for (int i = 0; i < MAX_FIREWORKS; i++) {
+        fireworks[i].explode();
+    }
+
+	glutPostRedisplay(); // redraw display
+}
+
+void myDisplay() {
+	glClear(GL_ACCUM_BUFFER_BIT); // clear the screen
+
+    glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// Displacement trick for exact pixelisation
+	glTranslatef(0.375, 0.375, 0);
+
+	// draw the fireworks
+    for (int i = 0; i < MAX_FRAMES; i++) {
+        draw_fireworks();
+        glAccum(GL_ACCUM, 1.0 / MAX_FRAMES);
+    }
 
     glAccum(GL_ACCUM, 0.99f);
+    glAccum(GL_RETURN, 1.0f);
 	glutSwapBuffers();
 
 	glutTimerFunc(1, timer, 0); // wait at least 1 ms before redrawing
@@ -95,7 +101,7 @@ void myMouse(int button, int state, int x, int y) {
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv); // initialize the Open-GL toolkit
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); // set the display mode
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_ACCUM); // set the display mode
 	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT); // set window size
 	glutInitWindowPosition(150, 100); // set window position
 	glutCreateWindow("Fireworks"); // open the screen window
